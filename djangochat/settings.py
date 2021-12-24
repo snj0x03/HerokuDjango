c8312664c299d1d73b7d6effb1ac30f3b4a310ab
sawnaysoe.mm@gmail.com <sawnaysoe.mm@gmail.com>
2021-12-24 18:36:54 +0630

    new file:   chat/__pycache__/__init__.cpython-39.pyc 	new file:   chat/__pycache__/admin.cpython-39.pyc 	new file:   chat/__pycache__/apps.cpython-39.pyc 	new file:   chat/__pycache__/models.cpython-39.pyc 	new file:   djangochat/__pycache__/__init__.cpython-39.pyc 	new file:   djangochat/__pycache__/settings.cpython-39.pyc 	modified:   djangochat/settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,11 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
